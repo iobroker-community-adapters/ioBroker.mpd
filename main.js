518d859e9c9cdb68473e6a59db30e4f78c653eea
Vladimir Vilisov <vvvalt@mail.ru>
2017-01-26 16:15:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -271,6 +271,7 @@ function _shift(){
     states['repeat'] = toBool(states['repeat']);
     states['random'] = toBool(states['random']);
 
+    adapter.log.debug('PLAY STATUS - ' + states.state);
     if (states.state === 'stop' || states.state === 'pause'){
         statePlay.isPlay = false;
         statePlay.sayid = null;
@@ -406,6 +407,7 @@ function sayit(command, val, t){
         } else {
             option.say.link = val;
         }
+        adapter.log.debug('statePlay = ' + JSON.stringify(statePlay));
         if (statePlay.isPlay && !statePlay.sayid && !t){
             option.cur = {
                 vol:   parseInt(states.volume, 10),
